0b536f14f4bc92a1aadfd18140ae5eb81794e56e
Yosiak-alv <Josias_romero60@outlook.com>
2024-04-22 21:53:13 -0600

    Update connections.py

--- config/connections.py
+++ config/connections.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 # Load .env file
 load_dotenv()
 
-def postgresql_connection():
+def postgresql_connection() -> str:
    return f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 
 
 #TODO check if the connection string is correct
-def msserver_connection():
+def msserver_connection() -> str:
    return f"mssql+pyodbc://{os.getenv('MSSERVER_USER')}:{os.getenv('MSSERVER_PASSWORD')}@{os.getenv('MSSERVER_HOST')}:{os.getenv('MSSERVER_PORT')}/{os.getenv('MSSERVER_DB')}?driver=ODBC+Driver+17+for+SQL+Server"
