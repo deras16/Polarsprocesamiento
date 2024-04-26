8ad23ed84e4f7b7e6e2e5ead7c6e479edc3f1c37
bocchiko <luisbonilla0908@gmail.com>
2024-04-26 11:30:38 -0600

    ETL Process for INSPRACTMAQ v1
    Also added Extract queries for "practicas" and "semillas"

--- src/INSPRACTMAQ/Inspractmaq.py
+++ src/INSPRACTMAQ/Inspractmaq.py
@@ -8,21 +8,48 @@ class Inspractmaq(Model):
     
     #TODO: check if the query is correct AND tipo column fill with the correct values
     def extract(self) -> pl.DataFrame:
-        query_insumos = """      
-            select  text as InsPracMaq, 'Insumo' Tipo from ws_dea.reusablecategoricaloptions r 
-                where r.questionnaireid = 'f3be9695-9847-4dfc-9f7d-b64790b029cf' and r.categoriesid = 'a117f4fa-973c-417c-9730-a67794f7a732'
+        query_INSPRACTMAQ = """      
+            select value as IdInsPracMaq, text as InsPracMaq, 'Practica' as Tipo from ws_dea.reusablecategoricaloptions r 
+            where r.questionnaireid = 'f3be9695-9847-4dfc-9f7d-b64790b029cf' and r.categoriesid = '573c5634-9e89-4d6c-b9df-b8ee6f59c93a'
+            union all select value as IdInsPracMaq, text as InsPracMaq, 'Insumo' as Tipo from ws_dea.reusablecategoricaloptions r 
+            where r.questionnaireid = 'f3be9695-9847-4dfc-9f7d-b64790b029cf' and r.categoriesid = 'a117f4fa-973c-417c-9730-a67794f7a732'
+            union all select value as IdInsPracMaq, text as InsPracMaq, 'Revisar' as Tipo 
+            from ws_dea.reusablecategoricaloptions r where r.questionnaireid = 'f3be9695-9847-4dfc-9f7d-b64790b029cf' and r.categoriesid = '71f41838-6868-4d2f-ad31-c830032893b4'
         """
-        query_maq_equipo = """
-            select text as InsPracMaq, 'Revisar' Tipo from ws_dea.reusablecategoricaloptions r 
-                where r.questionnaireid = 'f3be9695-9847-4dfc-9f7d-b64790b029cf' and r.categoriesid = '71f41838-6868-4d2f-ad31-c830032893b4'
-        """
-        df_insumos = pl.DataFrame(pl.read_database_uri(query=query_insumos, uri=self.postgres_connection, engine='connectorx'))
-        df_maq_equipo = pl.DataFrame(pl.read_database_uri(query=query_maq_equipo, uri=self.postgres_connection, engine='connectorx'))
-        
-        df = df_insumos.extend(df_maq_equipo)
+
+        df = pl.DataFrame(pl.read_database_uri(query=query_INSPRACTMAQ, uri=self.postgres_connection, engine='connectorx'))
+
         #make unique index id for each row
         ids = [ i for i in range(1, df.shape[0] + 1)]
         df = df.with_columns(pl.Series("IdInsPracMaq", ids))
-        print(df)
         return df
+    
+    def transform(self) -> pl.DataFrame:
+        df_extract = self.extract()
+        df_extract  = df_extract.with_columns(df_extract['IdInsPracMaq'].cast(pl.Int32), df_extract['inspracmaq'].cast(pl.Utf8), df_extract['tipo'].cast(pl.Utf8))
+        df_extract  = df_extract.rename({ "IdInsPracMaq": "IdInsPracMaq", "inspracmaq": "InsPracMaq", "tipo":"Tipo" })
+        
+        return df_extract
+    
+    def load(self):
+        #load on sql server
+        df_load = self.__validateData(self.transform())
+        if df_load.shape[0] > 0:
+            df_load.write_database(table_name="Inspractmaq", connection=self.mssql_connection, if_table_exists="append")
+            print('Inspractmaq Data loaded')
+        else:
+            print('No data to load')
+
+    def __validateData(self, df_transform) -> pl.DataFrame:
+        querySQLServer = """
+            select * from INSPRACTMAQ
+        """
+        df_sql_server = pl.DataFrame(pl.read_database_uri(query=querySQLServer, uri=self.mssql_connection, engine='connectorx'))
+        
+        #return only different rows on equal dataframes
+        df_diff = df_transform.join(df_sql_server, on="IdInsPracMaq", how="left")
+
+        #select only rows that are different
+        df_diff = df_diff.filter(pl.col("InsPracMaq_right").is_null()).select(pl.col("IdInsPracMaq"), pl.col("InsPracMaq"), pl.col("Tipo"))
+        return df_diff
         
